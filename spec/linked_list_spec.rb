1bfd89a149ef9f4869dbd116c892b9bcd7be0fc6
William Zale <williamzale@gmail.com>
2023-05-11 19:38:50 -0400

    Add test for count method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -4,11 +4,22 @@ require "./lib/node"
 RSpec.describe LinkedList do
   it "exists with readable attributes" do
     list = LinkedList.new
+
     expect(list.head).to eq(nil)
   end
   
   it "has an append method" do
     list = LinkedList.new
+    
     expect(list.append("doop")).to eq("doop")
+    expect(list).to eq(list)
+    expect(list.head.next_node).to eq(nil)
+  end
+  
+  it "has a count method" do
+    list = LinkedList.new
+    list.append("doop")
+
+    expect(list.count).to eq(1)
   end
 end
