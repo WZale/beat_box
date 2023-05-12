dc88594e970d9cf37122fb2133ada327bb12d2fe
William Zale <williamzale@gmail.com>
2023-05-12 19:33:16 -0400

    Add fleshed out version of to_string method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -31,16 +31,32 @@ class LinkedList
 
   def to_string
     current_node = @head
-    require 'pry'; binding.pry
+    node_data = + " "
     if current_node.next_node.nil?
-      head.data
+      @head.data
     else
-      
+      while current_node != nil
+        node_data << current_node.data + " "
+        current_node = current_node.next_node
+      end
+      node_data.strip 
     end
 
   end
 
-# the to_string method iterates through the linked list
-# and provides the data from each node as one joined string, unless
-# the head is pointing to nil (in which case only head would print).
+# The to_string method provides the data from each node, joined
+# together as a single string. It first creates two variables. Current_node
+# is set to equal head, or the beginning of the list. Node_data equals
+# an empty space string and the plus sign to allow for concatenation.
+
+# The method begins with an if conditional, checking to see if 
+# current_node.next_node is equal to nil (for a linked list of only one node.)
+# If so, it simply returns the data from head. However, if current_node does
+# not equal nil, we enter a while loop.
+
+# Inside that loop, current_node.data + " " is shovelled into node_data. Then,
+# current_node is set to equal current_node.next_node, advancing through the 
+# list. Once the end of the list (nil) is reached, the while loop ends, and
+# node_data is returned (with leading and trailing whitespaces removed).
+
 end
