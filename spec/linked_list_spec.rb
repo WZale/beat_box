ce8c47964574225dd5e8252612368b9846f1fdf6
William Zale <williamzale@gmail.com>
2023-05-12 17:54:17 -0400

    Add assertions for more nodes to be added for append and count tests

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -8,18 +8,30 @@ RSpec.describe LinkedList do
     expect(list.head).to eq(nil)
   end
   
-  it "has an append method" do
+  it "can append more than one node" do
     list = LinkedList.new
     
     expect(list.append("doop")).to eq("doop")
     expect(list).to eq(list)
+    expect(list.head).to be_an_instance_of(Node)
     expect(list.head.next_node).to eq(nil)
+
+    expect(list.append("deep")).to eq("deep")
+    expect(list.head.next_node).to be_an_instance_of(Node)
   end
   
   it "has a count method" do
     list = LinkedList.new
     list.append("doop")
-
-    expect(list.count).to eq(1)
+    list.append("deep")
+    expect(list.count).to eq(2)
+  end
+  
+  xit "has a to_string method" do
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+    
+    expect(list.to_string).to eq("doop deep")
   end
 end
