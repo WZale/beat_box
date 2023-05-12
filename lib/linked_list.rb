ee18006505c61909ef2f7a04237eda30d7a85244
William Zale <williamzale@gmail.com>
2023-05-12 17:52:06 -0400

    Add second version of count and append methods, and initial structure for newer to_string method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,21 +1,46 @@
 class LinkedList
   attr_reader :head
+
   def initialize()
     @head = nil
   end
 
   def append(data)
     node = Node.new(data)
-    @head = node
+    if  @head.nil?
+      @head = node
+    else 
+      @head.node_linker(node)
+    end
     node.data
   end
 
   def count
     node_counter = 0
-    if head.data != nil && head.next_node == nil
+    current_node = @head
+    while current_node != nil
       node_counter += 1
+      current_node = current_node.next_node
+      if current_node.next_node.nil?
+        node_counter += 1
+        break 
+      end
     end
     node_counter
   end
 
+  def to_string
+    current_node = @head
+    require 'pry'; binding.pry
+    if current_node.next_node.nil?
+      head.data
+    else
+      
+    end
+
+  end
+
+# the to_string method iterates through the linked list
+# and provides the data from each node as one joined string, unless
+# the head is pointing to nil (in which case only head would print).
 end
