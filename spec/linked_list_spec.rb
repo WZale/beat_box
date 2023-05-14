e9d1375c28afe5ba1c684faffd4c1313dd54582d
William Zale <williamzale@gmail.com>
2023-05-14 17:36:23 -0400

    Add tests for insert and find methods

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -4,7 +4,7 @@ require "./lib/node"
 RSpec.describe LinkedList do
   it "exists with readable attributes" do
     list = LinkedList.new
-
+    expect(list).to be_an_instance_of(LinkedList)
     expect(list.head).to eq(nil)
   end
   
@@ -40,7 +40,7 @@ RSpec.describe LinkedList do
   
   it "has an append method" do
     list = LinkedList.new
-
+    
     expect(list.append("plop")).to eq("plop")
     expect(list.to_string).to eq("plop")
     
@@ -50,5 +50,35 @@ RSpec.describe LinkedList do
     expect(list.to_string).to eq("dop plop suu")
     expect(list.count).to eq(3)
   end
+  
+  it "has an insert method" do
+    list = LinkedList.new
+    
+    expect(list.append("plop")).to eq("plop")
+    expect(list.to_string).to eq("plop")
+    expect(list.append("suu")).to eq("suu")
+    expect(list.prepend("dop")).to eq("dop")
+    
+    expect(list.to_string).to eq("dop plop suu")
+    expect(list.count).to eq(3)
+    
+    expect(list.insert(1, "woo")).to eq("woo")
+    expect(list.to_string).to eq("dop woo plop suu")
+  end
+  
+  it "has a find method" do
+    list = LinkedList.new
+    
+    expect(list.append("deep")).to eq("deep")
+    expect(list.append("woo")).to eq("woo")
+    expect(list.append("shi")).to eq("shi")
+    expect(list.append("shu")).to eq("shu")
+    expect(list.append("blop")).to eq("blop")
 
+    expect(list.to_string).to eq("deep woo shi shu blop")
+    
+    expect(list.find(2, 1)).to eq("shi")
+    expect(list.find(1, 3)).to eq("woo shi shu")
+
+  end
 end
