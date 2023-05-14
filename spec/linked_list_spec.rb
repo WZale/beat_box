a6bc62d8a56e754d4d3439afb0a9d595fd61f802
William Zale <williamzale@gmail.com>
2023-05-14 17:50:16 -0400

    Add test for includes? method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -79,6 +79,20 @@ RSpec.describe LinkedList do
     
     expect(list.find(2, 1)).to eq("shi")
     expect(list.find(1, 3)).to eq("woo shi shu")
+  end
+
+  it "has an includes? method" do
+    list = LinkedList.new
+    
+    expect(list.append("deep")).to eq("deep")
+    expect(list.append("woo")).to eq("woo")
+    expect(list.append("shi")).to eq("shi")
+    expect(list.append("shu")).to eq("shu")
+    expect(list.append("blop")).to eq("blop")
+
+    expect(list.to_string).to eq("deep woo shi shu blop")
+    expect(list.includes?("deep")).to eq(true)
+    expect(list.includes?("dep")).to eq(false)
 
   end
 end
