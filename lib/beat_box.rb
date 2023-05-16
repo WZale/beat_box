e21c220aad6c85816134dc77c2ff64a641dc1685
William Zale <williamzale@gmail.com>
2023-05-16 16:57:10 -0400

    feat: Add beginnings of beat validation to append method

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -7,8 +7,11 @@ class BeatBox
   end
 
   def append(node_data)
+    approved_beats = ["tee", "dee", "deep", "bop", "boop", "la", "na", "doo", "ditt", "woo", "hoo", "shu"]
     if node_data.nil?
       list.head
+    # elsif !approved_beats.include?(node_data)
+    #   require 'pry'; binding.pry
     else
       node_data_array = node_data.split.each do |node|
         list.append(node) 
@@ -23,5 +26,9 @@ class BeatBox
 
   def play
     `say -r 150 -v Good News "deep doo ditt woo hoo shu"`
+  end
+
+  def all
+    list.to_string
   end
 end
